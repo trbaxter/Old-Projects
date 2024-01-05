c522d7c9b02d1d1079a68c6322f8ca5718a19e9a
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-05 09:13:32 -0600

    Add files via upload

---
+++ Python/2018 Homework Assignments/Chapter 1/Chapter 1 - Exercise 3.py
@@ -0,0 +1,14 @@
+'''
+CH1 - Exercise 3
+Write a program that uses the letters F, U, and N to write out the word "FUN".
+'''
+
+print('''
+
+FFFFFFF    U        U    NN        NN
+FF               U        U    NNN     NN
+FFFFFFF    U        U    NN N    NN
+FF                U      U     NN  N   NN
+FF                 UUU       NN     NNN
+
+''')
