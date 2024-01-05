08b82cf74c10c8977fa64cf3702814bb51c69d94
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-05 13:03:43 -0600

    Update Ex05.py

--- Python/2018 Homework Assignments/Chapter 1/Ex05.py
+++ Python/2018 Homework Assignments/Chapter 1/Ex05.py
@@ -1,8 +1,4 @@
-'''
-CH1 - Exercise 5
-Write a program that displays the result of:
-(9.5 * 4.5 - 2.5 * 3)/(45.5-3.5)
-'''
+# Chapter 1 - Exercise 5
 
 print("The result of  (9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5) is:")
 print("\n")
