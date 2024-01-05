f767e2e3317ee880dab1ea3b02f3d0cf7e653c85
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-05 09:28:52 -0600

    fixed formatting

--- Python/2018 Homework Assignments/Chapter 1/Exercise 7.py
+++ Python/2018 Homework Assignments/Chapter 1/Exercise 7.py
@@ -7,7 +7,6 @@ Pi can be computed using the following formula:
 Write a program that displays the result of 4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11)
 
 and (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13 - 1/15)
-
 '''
 
 print("The result of the first formula is:")
