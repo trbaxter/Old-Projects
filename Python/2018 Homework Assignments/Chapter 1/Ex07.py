e73878783326f75fe1bc51ff6b2c620ce5226dd6
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-05 13:04:02 -0600

    Update Ex07.py

--- Python/2018 Homework Assignments/Chapter 1/Ex07.py
+++ Python/2018 Homework Assignments/Chapter 1/Ex07.py
@@ -1,13 +1,4 @@
-'''
-CH1 - Exercise 7
-Pi can be computed using the following formula:
-
-π = 4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + ...)
-
-Write a program that displays the result of 4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11)
-
-and (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13 - 1/15)
-'''
+# Chapter 1 - Exercise 7
 
 print("The result of the first formula is:")
 print(4*(1-(1/3)+(1/5)-(1/7)+(1/9)-(1/11)))
