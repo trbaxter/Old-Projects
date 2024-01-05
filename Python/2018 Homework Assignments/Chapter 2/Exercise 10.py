dca68d90a2a2468e0ad53bb8d20ca917fd9af8e3
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-05 11:56:22 -0600

    Add files via upload

---
+++ Python/2018 Homework Assignments/Chapter 2/Exercise 10.py
@@ -0,0 +1,20 @@
+'''
+Chapter 2 - Exercise 10
+Given an airplane's acceleration a and take-off speed v, compute the minimum runway length
+needed for an airplane to take off. Write a program that prompts the user to enter v in meters/second
+and the acceleration a in meters/second², and displays the minimum runway length.
+'''
+
+velocity, acceleration = eval(input("To determine the minimum runway length, \nplease enter the take-off speed (in m/s) and acceleration (in m/s²) separated by a comma: "))
+
+'''
+From kinematics, v_final^2 = v_initial^2 + 2a(distance)
+
+Assuming v_initial = 0 at takeoff:
+
+v_final^2 = 2a(distance)
+v_final^2/2a = distance
+'''
+
+distance = velocity**2 / 2*acceleration
+print("The required runway length is: ", distance, "m")
