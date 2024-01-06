9be2bb21142dd4421be66ef8b4d9fd1a0482934a
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-05 19:53:25 -0600

    Update Ex09.py

--- Python/2018 Homework Assignments/Chapter 2 [Elementary Programming]/Ex09.py
+++ Python/2018 Homework Assignments/Chapter 2 [Elementary Programming]/Ex09.py
@@ -5,13 +5,13 @@ import sys
 print("To calculate the wind-chill temperature, please provide the following below.\n")
 temp_f = eval(input("Enter a temperature between -58 °F and 41 °F: "))
 if temp_f < -58 or temp_f > 41:
-    print("Error: Temperature entered outside valid range.")
+    print("Error: Temperature entered is outside valid range.")
     sys.exit(0)
 # sys.exit(0) used to terminate the program upon bad entry with a custom error message
     
 windspeed = eval(input("Enter a wind speed (must be at least 2 miles per hour): "))
 if windspeed <2:
-    print("Error: Windspeed entered too low.")
+    print("Error: Windspeed entered is too low.")
     sys.exit(0)
 
 temp_wc = 35.74 + 0.6215*temp_f - 35.75*windspeed**(0.16) + 0.4275*temp_f*windspeed**(0.16)
