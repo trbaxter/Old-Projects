54feeb49528ea408a004b81c11cd762cb4c3a0d3
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-06 11:03:07 -0600

    Add files via upload

---
+++ Python/2018 Homework Assignments/Chapter 3 [Mathematical Functions, Strings, and Objects]/Ex09.py
@@ -0,0 +1,18 @@
+# Chapter 3 - Exercise 9
+
+import sys
+
+print("ASCII Character Finder\n")
+x_input = eval(input("Please enter a value between 0 and 127 to find the associated ASCII character: "))
+if x_input < 0 or x_input > 127:
+    print("Value entered is outside valid range. Please try again.")
+    sys.exit(0)
+
+x_rounded = round(x_input)
+# Input needs to be rounded to nearest whole integer since no ASCII characters have decimals
+x_str = str(x_rounded)
+# Input value returned as string for final display
+x_ascii = chr(x_rounded)
+# 
+
+print("The ASCII character for the value " + x_str + " is: " + x_ascii)
