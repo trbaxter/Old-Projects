6cbf3451a64ed589d0e38956bdb99b0e713a9eca
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-05 19:33:26 -0600

    Update Ex01.py

--- Python/2018 Homework Assignments/Chapter 2 [Elementary Programming]/Ex01.py
+++ Python/2018 Homework Assignments/Chapter 2 [Elementary Programming]/Ex01.py
@@ -1,8 +1,4 @@
-'''
-Chapter 2 - Exercise 1
-Write a program that reads a temperature in degrees Celsius from the console,
-converts it to degrees Fahrenheit, and displays the result. 
-'''
+# Chapter 2 - Exercise 1
 
 celsius_temp = eval(input("Enter a temperature in degrees Celsius: "))
 fahrenheit_temp = (9/5)*celsius_temp+32
