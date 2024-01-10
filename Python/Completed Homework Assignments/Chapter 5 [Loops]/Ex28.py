2d7d6a47146dc4a95309cda3dc5133bf7c666355
Tyler Baxter <108769032+trbaxter@users.noreply.github.com>
2024-01-09 19:03:19 -0600

    Add files via upload

---
+++ Python/Completed Homework Assignments/Chapter 5 [Loops]/Ex28.py
@@ -0,0 +1,20 @@
+# Chapter 5 - Exercise 28
+
+import math
+from decimal import Decimal, getcontext
+# Floating point numbers are insufficient for this problem, imported decimal for greater precision
+
+print("This program generates a numerical approximation of the mathematical constant e.")
+print("Warning: large numbers of decimal digits are used in this calculation to ensure accuracy. This will take a very long time to finish.")
+print()
+
+e = 0
+i = 0
+
+getcontext().prec = 100
+
+while i < 100001:
+    e += 1/Decimal(math.factorial(i))
+    i += 1
+    if i % 10000 == 0:
+        print("The approximation using", i, "iterations is:", e)
