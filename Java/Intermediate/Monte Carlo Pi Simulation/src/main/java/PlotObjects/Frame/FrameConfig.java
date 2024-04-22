d4d042078e6a073965c3c60100adf9a1052f57e2
SineThetaPrime <152728049+SineThetaPrime@users.noreply.github.com>
2024-04-22 12:07:17 -0500

    Added real-time visual update to MonteCarlo Simulation

--- Java/Intermediate/Monte Carlo Pi Simulation/src/main/java/PlotObjects/Frame/FrameConfig.java
+++ Java/Intermediate/Monte Carlo Pi Simulation/src/main/java/PlotObjects/Frame/FrameConfig.java
@@ -1,20 +1,17 @@
+package PlotObjects.Frame;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class SimFrame extends JFrame {
-
-    public SimFrame() {
+public class FrameConfig extends JFrame {
+    public FrameConfig() {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setAlwaysOnTop(true);
         setTitle("Monte Carlo Method of Approximating π");
-        setSize(500,500);
+        setSize(616, 639);
         setResizable(false);
         setLayout(new GridLayout());
         setLocationRelativeTo(null);
-
-        SimDrawing d = new SimDrawing();
-        add(d);
-        setVisible(true);
     }
-
 }
